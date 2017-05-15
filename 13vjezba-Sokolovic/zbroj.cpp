2ca0058d9195787361912b2696f58ef152f54157
markosokolovic7 <msokolovic21@gmail.com>
2017-05-15 09:26:52 +0200

    ispisuje se glavni izbornik

---
+++ 13vjezba-Sokolovic/zbroj.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Glavni izbornik"<< endl;
+    cout << "1. Unos nove osobe"<< endl;
+    cout << "2. Ispisi podatke"<< endl;
+    cout << "3. Pretraga prema racunu"<< endl;
+    cout << "4. Pretraga prema prezimenu"<< endl;
+    cout << "5. Ispisi sortirano"<< endl;
+    int izbor;
+    cin>>izbor;
+
+
+}
